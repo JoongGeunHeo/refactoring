585878572edf9bd5efadd4766d5f849c83fa1528
JoongGeun Heo <jgheo78@gmail.com>
2021-11-21 11:34:02 +0900

    Separate Functions and Files

--- statement_refactoring.js
+++ statement_refactoring.js
@@ -1,4 +1,9 @@
-'use strict';
+//'use strict';
+
+import createStatementData from './createStatementData_refactoring.js';
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 const fs = require('fs');
 let rawdata_invoices = fs.readFileSync('invoices.json');
@@ -7,44 +12,47 @@ let invoices = JSON.parse(rawdata_invoices);
 let plays = JSON.parse(rawdata_plays);
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumnCredits = 0;
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US",
-                        { style: "currency", currency: "USD",
-                        minimumFactionDigits: 2}).format;
-    
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = 0;
+    return renderPlainText(createStatementData(invoice, plays));
+}
 
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience - 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${paly.type}`);
-        }
-        volumnCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === play.type) volumnCredits += Math.floor(perf.audience / 5);
+function renderPlainText(data) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
-        totalAmount += thisAmount;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
     }
-    result += `총액: ${format(totalAmount/100)}\n`;
-    result += `적립 포인트: ${volumnCredits}점\n`;
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumnCredits}점\n`;
     return result;
+}
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumnCredits}</em>점</p>\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+        { style: "currency", currency: "USD",
+        minimumFactionDigits: 2}).format(aNumber/100);
 }
 
 let result = statement(invoices[0], plays);
 console.log(result);
+
+let resultHtml = htmlStatement(invoices[0], plays);
+console.log(resultHtml);
